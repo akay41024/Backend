c71b87c6cad8aac465d49461727ea431d974bf92
Aasif khan <akay41024@gmail.com>
2024-05-01 17:36:41 +0530

    initial commit

---
+++ Test/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5000
+require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('Twitter Yaha hai');
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Login Yaha hai</h1>');
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
