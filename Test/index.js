921b699e6fa5577e12020fb07b8ad1262708648b
Aasif khan <akay41024@gmail.com>
2024-05-05 18:14:49 +0530

    some changes

--- Test/index.js
+++ Test/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
+import express from 'express'
+import dotenv from "dotenv";
+
 const app = express()
-const port = 5000
-require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+dotenv.config({
+  path: "./env",
+});
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server is in running state !!!')
 })
 
 app.get('/twitter', (req, res) => {
@@ -16,6 +22,6 @@ app.get('/login', (req, res) => {
   res.send('<h1>Login Yaha hai</h1>');
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
