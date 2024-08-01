2a7f49a7e26a636a6341838a7cce057b93f8a59b
Aasif khan <akay41024@gmail.com>
2024-08-01 22:27:30 +0530

    feat: Add comment, like, playlist, and tweet models and controllers

--- mega-project/src/routes/user.routes.js
+++ mega-project/src/routes/user.routes.js
@@ -43,7 +43,7 @@ router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
 
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
